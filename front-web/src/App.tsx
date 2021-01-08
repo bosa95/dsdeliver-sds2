f425d224b6e60389003cc2d0ff50f2615147f129
Matheus Bosa Mischiatti <matheusbosa95@gmail.com>
2021-01-08 12:15:36 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,14 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Routes from './routes';
 import './App.css';
+
 
 function App() {
   return (
     <>
       <Routes/>
+      <ToastContainer />
     </>
   );
 }
